a2443478f14c9df57b8c507bf9580a8801b81fb8
Quang Chien <qchien2406@gmail.com>
2018-04-06 12:27:54 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'containers/App/App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'assets/css/sticky-footer-navbar.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
