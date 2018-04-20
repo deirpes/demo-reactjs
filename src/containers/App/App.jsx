8e9e4b559c93760a302f189c176402c57075900e
Quang Chien <quangchien@mail.com>
2018-04-20 22:46:05 +0700

    Put layout structure into folder layout

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Router, Route, browserHistory } from 'react-router';
-import Layout from 'containers/Layout/Layout.jsx';
+import Layout from 'layouts/Layout.jsx';
 import Home from 'views/Home/Home.jsx';
 import StudentsList from 'views/Students/List.jsx';
 import StudentsAdd from 'views/Students/Add.jsx';
