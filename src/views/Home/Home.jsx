43e84dea57d5c1391ee6219e224394067d16bd58
Chien Nguyen <deirpes@gmail.com>
2019-04-06 22:32:31 +0700

    Change some text

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,7 +4,7 @@ import { Title } from "components"
 const Home = () => (
     <div>
         <Title name="Home Page"/>
-        <p className="lead">Hello there ! this is a demo of crud students</p>
+        <p className="lead">Hellos there ! this is a demo of crud students</p>
         <p>Go to <Link to="/students">Students</Link> page</p>
     </div>
 )
